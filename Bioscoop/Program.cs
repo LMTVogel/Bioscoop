7c235d9acef3824db03756de81b4795f163a09c1
Luuk <lmt.vogel@student.avans.nl>
2024-02-04 14:03:54 +0100

    Created the unit tests project

--- Bioscoop/Program.cs
+++ Bioscoop/Program.cs
@@ -7,21 +7,21 @@ class Program
     static void Main(string[] args)
     {
         Movie movie = new("The Matrix");
-        MovieScreening movieScreening = new(movie, new DateTime(2024, 2, 2, 20, 0, 0), 10);
+        MovieScreening movieScreening = new(movie, new DateTime(2024, 2, 4, 20, 0, 0), 10);
         MovieTicket movieTicket1 = new(movieScreening, 1, 1, true);
         MovieTicket movieTicket2 = new(movieScreening, 1, 2, false);
-        MovieTicket movieTicket3 = new(movieScreening, 1, 3, true);
+        MovieTicket movieTicket3 = new(movieScreening, 1, 3, false);
         MovieTicket movieTicket4 = new(movieScreening, 1, 4, false);
         MovieTicket movieTicket5 = new(movieScreening, 1, 5, true);
         MovieTicket movieTicket6 = new(movieScreening, 1, 6, false);
-        Order order = new(1, false);
+        Order order = new(1, true);
 
         order.AddSeatReservation(movieTicket1);
         order.AddSeatReservation(movieTicket2);
-        order.AddSeatReservation(movieTicket3);
-        order.AddSeatReservation(movieTicket4);
-        order.AddSeatReservation(movieTicket5);
-        order.AddSeatReservation(movieTicket6);
+        // order.AddSeatReservation(movieTicket3);
+        // order.AddSeatReservation(movieTicket4);
+        // order.AddSeatReservation(movieTicket5);
+        // order.AddSeatReservation(movieTicket6);
 
         Console.WriteLine(order.CalculatePrice());
         order.Export(TicketExportFormat.JSON);
